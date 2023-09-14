42d12bbdf33be69d535238dc325909dffbfed29b
MarkedOliv <olivdev4@gmail.com>
2023-09-14 18:49:43 -0400

    funcionalidad de el login y catch de errores para mostrarlos con sweetalert2

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+
+import Swal from 'sweetalert2';
 
 @Component({
   templateUrl: './login-page.component.html',
@@ -7,6 +10,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginPageComponent {
   private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
 
   public myForm: FormGroup = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
@@ -14,6 +18,13 @@ export class LoginPageComponent {
   });
 
   login() {
-    console.log(this.myForm.value);
+    const { email, password } = this.myForm.value;
+
+    this.authService.login(email, password).subscribe({
+      next: () => console.log('Todo salió bien!! :)'),
+      error: (message) => {
+        Swal.fire('Error', message, 'error');
+      },
+    });
   }
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,11 +1,11 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject, signal } from '@angular/core';
+import { Injectable, computed, inject, signal } from '@angular/core';
 
-import { Observable, of } from 'rxjs';
+import { Observable, catchError, map, tap, throwError } from 'rxjs';
 
 import { environment } from 'src/environments/environments';
 
-import { User, AuthStatus } from '../interfaces';
+import { User, AuthStatus, LoginResponse } from '../interfaces';
 
 @Injectable({
   providedIn: 'root',
@@ -18,9 +18,29 @@ export class AuthService {
   private _currentUser = signal<User | null>(null);
   private _authStatus = signal<AuthStatus>(AuthStatus.checking);
 
+  public currentUser = computed(() => this._currentUser());
+  public authStatus = computed(() => this._authStatus());
+
   constructor() {}
 
   login(email: string, password: string): Observable<boolean> {
-    return of(true);
+    const url = `${this.baseUrl}/auth/login`;
+    const body = { email, password };
+
+    return this.http.post<LoginResponse>(url, body).pipe(
+      tap(({ user, token }) => {
+        this._currentUser.set(user);
+        this._authStatus.set(AuthStatus.authenticated);
+
+        localStorage.setItem(environment.TOKEN, token);
+
+        console.log({ user, token });
+      }),
+      map(() => true),
+      catchError((err) => {
+        console.log(err);
+        return throwError(() => err.error.message)
+      })
+    );
   }
 }
