94b2398da07a0e128f978b90e12acafb950bf072
MarkedOliv <olivdev4@gmail.com>
2023-09-14 20:06:51 -0400

    finalización de la sección 26, entendiendo los guards para iniciar y manejar sesiones de usuarios

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,8 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 import Swal from 'sweetalert2';
+
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   templateUrl: './login-page.component.html',
@@ -11,17 +13,18 @@ import Swal from 'sweetalert2';
 export class LoginPageComponent {
   private fb = inject(FormBuilder);
   private authService = inject(AuthService);
+  private router = inject(Router);
 
   public myForm: FormGroup = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
+    email: ['olivdev@gmail.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]],
   });
 
   login() {
     const { email, password } = this.myForm.value;
 
     this.authService.login(email, password).subscribe({
-      next: () => console.log('Todo salió bien!! :)'),
+      next: () => this.router.navigateByUrl('/dashboard'),
       error: (message) => {
         Swal.fire('Error', message, 'error');
       },
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,16 +1,24 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, computed, inject, signal } from '@angular/core';
 
-import { Observable, catchError, map, tap, throwError } from 'rxjs';
+import { Observable, catchError, map, of, tap, throwError } from 'rxjs';
 
 import { environment } from 'src/environments/environments';
 
-import { User, AuthStatus, LoginResponse } from '../interfaces';
+import {
+  User,
+  AuthStatus,
+  LoginResponse,
+  CheckTokenResponse,
+} from '../interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  constructor() {
+    this.checkAuthStatus().subscribe();
+  }
   private readonly baseUrl: string = environment.baseUrl;
 
   private http = inject(HttpClient);
@@ -21,25 +29,49 @@ export class AuthService {
   public currentUser = computed(() => this._currentUser());
   public authStatus = computed(() => this._authStatus());
 
-  constructor() {}
+  private setAuthentication(user: User, token: string): boolean {
+    this._currentUser.set(user);
+    this._authStatus.set(AuthStatus.authenticated);
+
+    localStorage.setItem(environment.TOKEN, token);
+
+    return true;
+  }
 
   login(email: string, password: string): Observable<boolean> {
     const url = `${this.baseUrl}/auth/login`;
     const body = { email, password };
 
     return this.http.post<LoginResponse>(url, body).pipe(
-      tap(({ user, token }) => {
-        this._currentUser.set(user);
-        this._authStatus.set(AuthStatus.authenticated);
-
-        localStorage.setItem(environment.TOKEN, token);
-
-        console.log({ user, token });
-      }),
-      map(() => true),
+      map(({ user, token }) => this.setAuthentication(user, token)),
       catchError((err) => {
-        console.log(err);
-        return throwError(() => err.error.message)
+        return throwError(() => err.error.message);
+      })
+    );
+  }
+
+  logout(): void {
+    this._currentUser.set(null);
+    this._authStatus.set(AuthStatus.notAuthenticated);
+    localStorage.removeItem(environment.TOKEN);
+  }
+
+  checkAuthStatus(): Observable<boolean> {
+    const url = `${this.baseUrl}/auth/check-token`;
+    const token = localStorage.getItem(environment.TOKEN);
+
+    if (!token) {
+      this.logout();
+      return of(false);
+    }
+
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    return this.http.get<CheckTokenResponse>(url, { headers }).pipe(
+      map(({ user, token }) => this.setAuthentication(user, token)),
+      catchError(() => {
+        this._authStatus.set(AuthStatus.notAuthenticated);
+        return of(false);
       })
     );
   }
